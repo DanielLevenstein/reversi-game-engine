1269ae2e148a92c9ad5ff48974b16498005a5849
DanielLevenstein <danielalevenstein@gmail.com>
2025-03-26 11:20:39 -0500

    Added unit test for ReversiBoard and refactored class: Change variable name of gameBoardVar to gameBoardStr  Create virtual getters and setters for piece values.

--- src/main/java/org/esotericcode/reversi/gameengine/reversigameengine/model/ReversiBoard.java
+++ src/main/java/org/esotericcode/reversi/gameengine/reversigameengine/model/ReversiBoard.java
@@ -7,35 +7,79 @@ import java.util.Arrays;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Data
+//@Document
 public class ReversiBoard {
     private @Id @GeneratedValue Long id;
-    @Lob
-    private String gameBoardVals;
 
-    public char[][] getGameBoardValsAsArray() {
-        return Arrays.stream(gameBoardVals.split(";"))
+    @Setter
+    @Getter
+    @Lob
+    private String gameBoardStr;
+
+    public char[][] getGameBoardAsArray() {
+        return Arrays.stream(gameBoardStr.split(";"))
                      .map(String::toCharArray)
                      .toArray(char[][]::new);
     }
 
-    public void setGameBoardValsAsArray(char[][] gameBoardArray) {
-        this.gameBoardVals = Arrays.stream(gameBoardArray)
+    public void setGameBoardAsArray(char[][] gameBoardArray) {
+        this.gameBoardStr = Arrays.stream(gameBoardArray)
                                    .map(String::new)
                                    .reduce((row1, row2) -> row1 + "\n" + row2)
                                    .orElse("");
     }
 
-    public String getPieceAt(int x, int y) {
-        return this.gameBoardVals.split("\n")[x].charAt(y) + "";
-    }
-    public ReversiBoard() {}
+    public char getPiece(int x, int y) {
 
-    public ReversiBoard(char [][] gameBoardVals) {
-        this.setGameBoardValsAsArray(gameBoardVals);
-        System.out.println(this.toString());
+        int charIndex = (x * 9) + y;
+        return this.gameBoardStr.charAt(charIndex);
+    }
+
+    public void setPiece(int x, int y, char piece) {
+        // Calculate the index of the char to replace
+        int charIndex = (x * 9) + y;
+//        this.gameBoardStr = this.gameBoardStr.substring(0, charIndex) + "X" + this.gameBoardStr.substring(charIndex + 1);
+        var sb = new StringBuilder(this.gameBoardStr);
+        sb.setCharAt(charIndex, piece);
+        this.gameBoardStr = sb.toString();
+    }
+
+    public ReversiBoard() {
+        this.gameBoardStr = this.getEmptyBoard();
+    }
+
+    public ReversiBoard(char [][] gameBoardArray) {
+        this.setGameBoardAsArray(gameBoardArray);
+    }
+
+    public ReversiBoard(String gameBoardStr) {
+        this.gameBoardStr = gameBoardStr;
+    }
+
+    public String getEmptyBoard() {
+        StringBuilder emptyBoard = new StringBuilder();
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 8; j++) {
+                if(i == 3 && j == 3) {
+                    emptyBoard.append("X");
+                } else if(i == 3 && j == 4) {
+                    emptyBoard.append("O");
+                } else if(i == 4 && j == 3) {
+                    emptyBoard.append("O");
+                } else if(i == 4 && j == 4) {
+                    emptyBoard.append("X");
+                } else {
+                    emptyBoard.append(" ");
+                }
+            }
+            emptyBoard.append("\n");
+        }
+        return emptyBoard.toString();
     }
 
     public String toString() {
@@ -44,7 +88,7 @@ public class ReversiBoard {
         for(int i = 0; i < 8; i++) {
             prettyBoard.append("|");
             for(int j = 0; j < 8; j++) {
-                prettyBoard.append(this.getPieceAt(i,j)).append("|");
+                prettyBoard.append(this.getPiece(i,j)).append("|");
             }
             prettyBoard.append("\n");
         }
--- src/test/java/org/esotericcode/reversi/gameengine/reversigameengine/ReversiGameEngineApplicationTests.java
+++ src/test/java/org/esotericcode/reversi/gameengine/reversigameengine/ReversiGameEngineApplicationTests.java
@@ -1,13 +1,60 @@
 package org.esotericcode.reversi.gameengine.reversigameengine;
 
+import org.esotericcode.reversi.gameengine.reversigameengine.model.ReversiBoard;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ReversiGameEngineApplicationTests {
 
     @Test
-    void contextLoads() {
+    void testEmptyBoard() {
+        ReversiBoard board = new ReversiBoard();
+        assertEquals('X', board.getPiece(3,3));
+        assertEquals('O', board.getPiece(3,4));
+        assertEquals('O', board.getPiece(4,3));
+        assertEquals('X', board.getPiece(4,4));
+        assert(board.toString().contains("|X|O|"));
+        assert(board.toString().contains("|O|X|"));
+        System.out.println(board);
+    }
+    @Test
+    void testEmptyBoardToString() {
+        ReversiBoard board = new ReversiBoard();
+        String[] boardRowsStr = board.toString().split("\n");
+        assertEquals("-_-_-_-_-_-_-_-_-",boardRowsStr[0]);
+        assertEquals("| | | | | | | | |",boardRowsStr[1]);
+        assertEquals("| | | | | | | | |",boardRowsStr[2]);
+        assertEquals("| | | | | | | | |",boardRowsStr[3]);
+        assertEquals("| | | |X|O| | | |",boardRowsStr[4]);
+        assertEquals("| | | |O|X| | | |",boardRowsStr[5]);
+        assertEquals("| | | | | | | | |",boardRowsStr[6]);
+        assertEquals("| | | | | | | | |",boardRowsStr[7]);
+        assertEquals("| | | | | | | | |",boardRowsStr[8]);
+        assertEquals("-_-_-_-_-_-_-_-_-",boardRowsStr[9]);
     }
 
+
+    @Test
+    void testSetPiece(){
+        ReversiBoard board = new ReversiBoard();
+        board.setPiece(3,5,'X');
+        assertEquals('X', board.getPiece(3,5));
+
+        String[] boardRowsStr = board.toString().split("\n");
+        assertEquals("-_-_-_-_-_-_-_-_-",boardRowsStr[0]);
+        assertEquals("| | | | | | | | |",boardRowsStr[1]);
+        assertEquals("| | | | | | | | |",boardRowsStr[2]);
+        assertEquals("| | | | | | | | |",boardRowsStr[3]);
+        assertEquals("| | | |X|O|X| | |",boardRowsStr[4]);
+        assertEquals("| | | |O|X| | | |",boardRowsStr[5]);
+        assertEquals("| | | | | | | | |",boardRowsStr[6]);
+        assertEquals("| | | | | | | | |",boardRowsStr[7]);
+        assertEquals("| | | | | | | | |",boardRowsStr[8]);
+        assertEquals("-_-_-_-_-_-_-_-_-",boardRowsStr[9]);
+    }
 }
