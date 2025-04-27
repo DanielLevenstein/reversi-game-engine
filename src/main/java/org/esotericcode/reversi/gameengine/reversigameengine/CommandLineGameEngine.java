3e10e19dcc7afa0185d1496a0637c2358c21236b
DanielLevenstein <danielalevenstein@gmail.com>
2025-04-26 20:48:34 -0500

    Rename ReversiGameEngineApplication.java to CommandLineGameEngine.java

--- src/main/java/org/esotericcode/reversi/gameengine/reversigameengine/CommandLineGameEngine.java
+++ src/main/java/org/esotericcode/reversi/gameengine/reversigameengine/CommandLineGameEngine.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import java.util.Arrays;
 
 @Component
-public class ReversiGameEngineApplication{
+public class CommandLineGameEngine{
 
     public static void main(String[] args) {
         if (args.length > 0 && args[0].equals("--command-line")) {
