cb7bbb83309d64d23f0f9fda1cce9fb3897b51e6
DanielLevenstein <danielalevenstein@gmail.com>
2025-04-26 20:36:29 -0500

    Remove ; char from Scala code.

--- src/main/scala/org/esotericcode/reversi/gameengine/reversigameengine/model/ImmutableReversiBoard.scala
+++ src/main/scala/org/esotericcode/reversi/gameengine/reversigameengine/model/ImmutableReversiBoard.scala
@@ -1,6 +1,6 @@
 package org.esotericcode.reversi.gameengine.reversigameengine.model
 
-import org.esotericcode.reversi.gameengine.reversigameengine.model.ImmutableReversiBoard.{getPieceAlgebra, isOpponent, onBoard};
+import org.esotericcode.reversi.gameengine.reversigameengine.model.ImmutableReversiBoard.{getPieceAlgebra, isOpponent, onBoard}
 object ImmutableReversiBoard {
 
   private def onBoard(x: Int, y: Int) = {
@@ -8,8 +8,8 @@ object ImmutableReversiBoard {
   }
 
   def getPieceAlgebra(letterIndex: Int, numIndex: Int): String = {
-    val invertedNum = 8 - numIndex;
-    ('A' + letterIndex).asInstanceOf[Char].toString + ( '0' + invertedNum).asInstanceOf[Char].toString;
+    val invertedNum = 8 - numIndex
+    ('A' + letterIndex).asInstanceOf[Char].toString + ( '0' + invertedNum).asInstanceOf[Char].toString
   }
   def isOpponent(neighborColor: Char, player: Char): Boolean = neighborColor != player && neighborColor != ' '
 }
@@ -17,21 +17,21 @@ object ImmutableReversiBoard {
 class ImmutableReversiBoard(val gameBoard: String) {
 
   def getPiece(letterIndex: Int, numIndex: Int): Char = {
-    val charIndex = (numIndex * 9) + letterIndex;
-    gameBoard.charAt(charIndex);
+    val charIndex = (numIndex * 9) + letterIndex
+    gameBoard.charAt(charIndex)
   }
 
   def isEmptySpace(ix: Int, iy: Int): Boolean = {
-    this.getPiece(ix, iy) == ' ';
+    this.getPiece(ix, iy) == ' '
   }
 
   def isValidMove(algebraicNotation: String, player: Char): Boolean = {
-    isValidMove(algebraicNotation.charAt(0) - 'A', 8 - (algebraicNotation.charAt(1) - '0'), player);
+    isValidMove(algebraicNotation.charAt(0) - 'A', 8 - (algebraicNotation.charAt(1) - '0'), player)
   }
   private def isValidMove(ix: Int, iy:Int, player: Char): Boolean = {
 
     if (!this.isEmptySpace(ix, iy)) {
-      return false;
+      return false
     }
     // Check 9 surrounding spaces for token belonging to other player.
     val neighbor = for {
@@ -42,34 +42,34 @@ class ImmutableReversiBoard(val gameBoard: String) {
       (x, y)
     }
 
-    var isMove = false;
+    var isMove = false
     neighbor.iterator.foreach { case (x, y) =>
-      var neighborColor = this.getPiece(x, y);
+      var neighborColor = this.getPiece(x, y)
       if (isOpponent(neighborColor, player)) {
-        val xOffset = x - ix;
-        val yOffset = y - iy;
-        var xVal = x;
-        var yVal = y;
+        val xOffset = x - ix
+        val yOffset = y - iy
+        var xVal = x
+        var yVal = y
         while (onBoard(xVal, yVal) && isOpponent(neighborColor, player)) {
-          xVal = xVal + xOffset;
-          yVal = yVal + yOffset;
+          xVal = xVal + xOffset
+          yVal = yVal + yOffset
           neighborColor = this.getPiece(xVal, yVal)
         }
         if (neighborColor == player) {
-          isMove = true;
+          isMove = true
         }
       }
     }
-    isMove;
+    isMove
   }
 
   def getValidMoves(player: Char): Seq[String] = {
     val validMoves = for {
-      i <- 0 to 7;
-      j <- 0 to 7;
+      i <- 0 to 7
+      j <- 0 to 7
       if (isValidMove(i, j, player))
     }
     yield { getPieceAlgebra(i, j)}
-    validMoves;
+    validMoves
   }
 }
